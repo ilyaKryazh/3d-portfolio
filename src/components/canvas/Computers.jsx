de9f3cf1389b1c03a942f399e75101ccdf293a34
ilyaKryazh <ilya.kryazik10@gmail.com>
2023-12-18 20:03:20 +0300

    Create "Hero" component

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -1,9 +1,50 @@
-import React from 'react'
+import { Suspense, useEffect, useState } from 'react'
+import { Canvas } from '@react-three/fiber'
+import { OrbitControls, Stats, Preload, useGLTF } from '@react-three/drei'
+import CanvasLoader from '../Loader'
+import { wrap } from 'framer-motion'
 
-const Computers = () => {
+const Computers = ({ isMobile }) => {
+  const computer = useGLTF('/desktop_pc/scene.gltf')
+
   return (
-    <div>Computers</div>
+    <mesh>
+      <hemisphereLight intensity={0.15} groundColor='black' />
+      <pointLight intensity={6} />
+      <spotLight position={[-20, 50, 10]} angle={0.12} intensity={10000} />
+      <primitive object={computer.scene} scale={isMobile ? 0.4 : 0.75} position={isMobile ? [0, -3, -2.2] : [0, -3.25, -1.5]} rotation={[-0.01, -0.2, -0.1]} />
+    </mesh>
   )
 }
 
-export default Computers
+const ComputerCanvas = () => {
+  const [isMobile, setIsMobile] = useState(false)
+  useEffect(() => {
+    const mediaQuery = window.matchMedia('(max-width: 500px)')
+
+    setIsMobile(mediaQuery.matches)
+
+    const handleMediaQueryChange = (event) => {
+      setIsMobile(event.matches)
+    }
+
+    mediaQuery.addEventListener('change', handleMediaQueryChange)
+
+    return () => {
+      mediaQuery.removeEventListener('change', handleMediaQueryChange)
+    }
+  }, [])
+
+  return (
+    <Canvas frameloop='demand' shadows camera={{ position: [20, 3, 5], fov: 25 }} gl={{ preserveDrawingBuffer: true }}>
+      <Suspense fallback={<CanvasLoader />}>
+        <OrbitControls enableZoom={false} enablePan={false} minPolarAngle={Math.PI / 2} maxPolarAngle={Math.PI / 2} />
+        <Stats />
+        <Computers isMobile={isMobile} />
+      </Suspense>
+      <Preload all />
+
+    </Canvas>
+  )
+}
+export default ComputerCanvas
